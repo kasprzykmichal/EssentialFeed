1fb618e59cfc0b12578cfffbfde32bc01964659a
Michał Kasprzyk <kasprzyk.michal@me.com>
2022-12-18 17:05:23 +0100

    Separate CoreData model classes from their helper methods and properties

--- EssentialFeed/Feed Cache/Infrastructure/ManagedCache.swift
+++ EssentialFeed/Feed Cache/Infrastructure/ManagedCache.swift
@@ -11,17 +11,19 @@ import CoreData
 internal class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
-
-    var localFeed: [LocalFeedImage] {
+}
+    
+extension ManagedCache {
+    internal var localFeed: [LocalFeedImage] {
         return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }
 
-    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+    internal static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
         try find(in: context).map(context.delete)
         return ManagedCache(context: context)
     }
 
-    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+    internal static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
         request.returnsObjectsAsFaults = false
         return try context.fetch(request).first
