3c417dd58a79db6fc8519078a96d8f255503c8ce
Michał Kasprzyk <kasprzyk.michal@me.com>
2022-12-22 11:54:29 +0100

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Michał Kasprzyk on 22/12/2022.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
