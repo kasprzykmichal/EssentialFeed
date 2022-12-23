eabb3e60188d9065977f539fbe78ef4ad14e9015
Michał Kasprzyk <kasprzyk.michal@me.com>
2022-12-23 18:43:27 +0100

    MoveFeedImageLoader to FeedImageViewModel and remove feed image loading dependencies from FeedImageCellController

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -5,6 +5,7 @@
 //  Created by Michał Kasprzyk on 22/12/2022.
 //
 
+import UIKit
 import EssentialFeed
 
 public final class FeedUIComposer {
@@ -21,7 +22,7 @@ public final class FeedUIComposer {
     private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
         return { [weak controller] feed in
             controller?.tableModel = feed.map { model in
-                FeedImageCellController(model: model, imageLoader: loader)
+                FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
             }
         }
     }
---
+++ EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Michał Kasprzyk on 23/12/2022.
+//
+
+import EssentialFeed
+
+final class FeedImageViewModel<Image> {
+    typealias Observer<T> = (T) -> Void
+    
+    private var task: FeedImageDataLoaderTask?
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    private let imageTransformer: (Data) -> Image?
+
+    var onImageLoad: Observer<Image>?
+    var onImageLoadingStateChange: Observer<Bool>?
+    var onShouldRetryImageLoadStateChange: Observer<Bool>?
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
+        self.model = model
+        self.imageLoader = imageLoader
+        self.imageTransformer = imageTransformer
+    }
+
+    var hasLocation: Bool {
+        return model.location != nil
+    }
+
+    var location: String? {
+        return model.location
+    }
+
+    var description: String? {
+        return model.description
+    }
+
+    func loadImageData() {
+        onImageLoadingStateChange?(true)
+        onShouldRetryImageLoadStateChange?(false)
+        task = imageLoader.loadImageData(from: self.model.url) { [weak self] result in
+            self?.handle(result)
+        }
+    }
+
+    private func handle(_ result: FeedImageDataLoader.Result) {
+        if let image = (try? result.get()).flatMap(imageTransformer) {
+            onImageLoad?(image)
+        } else {
+            onShouldRetryImageLoadStateChange?(true)
+        }
+        onImageLoadingStateChange?(false)
+    }
+
+    func cancelImageDataLoad() {
+        task?.cancel()
+    }
+}
+
