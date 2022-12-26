bcbcc29c810feefbd5464d86434001f49482c3e8
Michał Kasprzyk <kasprzyk.michal@me.com>
2022-12-26 13:41:56 +0100

    Extract FeedErrorViewModel to separate file

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-public protocol FeedErrorView {
-    func display(_ viewModel: FeedErrorViewModel)
-}
-
-public struct FeedErrorViewModel {
-    public let message: String?
-    
-    public static let noError = FeedErrorViewModel(message: nil)
-    
-    public static func error(message: String) -> FeedErrorViewModel {
-        return FeedErrorViewModel(message: message)
-    }
-}
-
 public protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)
 }
