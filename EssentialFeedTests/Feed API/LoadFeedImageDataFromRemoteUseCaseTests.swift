224f5368d84855b91a6199452f806b94c67b70e2
Michał Kasprzyk <kasprzyk.michal@me.com>
2022-12-27 00:53:22 +0100

    Move anyData() to SharedTestHelpers

--- EssentialFeedTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -177,10 +177,6 @@ final class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
         return (sut, client)
     }
 
-    private func anyData() -> Data {
-        return Data("any data".utf8)
-    }
-
     private func failure(_ error: RemoteFeedImageDataLoader.Error) -> FeedImageDataLoader.Result {
         return .failure(error)
     }
