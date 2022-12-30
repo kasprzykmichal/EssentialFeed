3ea517fa365e72049a6f224d546918a3cf8ea814
Michał Kasprzyk <kasprzyk.michal@me.com>
2022-12-30 19:32:45 +0100

    Render error message on load feed error

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -293,6 +293,16 @@ final class FeedUIIntegrationTests: XCTestCase {
 
         XCTAssertEqual(sut.errorMessage, nil)
     }
+
+    func test_loadFeedCompletion_rendersErrorMessageOnError() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(sut.errorMessage, nil)
+
+        loader.completeFeedLoadingWithError(at: 0)
+        XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+    }
     
     // MARK: - Helpers
 
