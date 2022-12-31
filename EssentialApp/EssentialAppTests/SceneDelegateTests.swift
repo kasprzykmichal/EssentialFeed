9892cb4477f5e62bb40adb5c90d8076992c24b37
Michał Kasprzyk <kasprzyk.michal@me.com>
2022-12-31 11:06:14 +0100

    Improve test name

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -19,7 +19,7 @@ class SceneDelegateTests: XCTestCase {
         XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected window to be the key window")
     }
     
-    func test_sceneWillConnectToSession_configuresRootViewController() {
+    func test_configureWindow_configuresRootViewController() {
         let sut = SceneDelegate()
 
         sut.window = UIWindow()
