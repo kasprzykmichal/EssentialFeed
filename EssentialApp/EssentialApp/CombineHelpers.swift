2bf0b32280aef7beb5402183dd5405b692e5f348
Michał Kasprzyk <kasprzyk.michal@me.com>
2023-01-10 11:41:15 +0100

    Replace remote loader composition with HTTPClient publisher composed with FeedItemsMapper

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -10,6 +10,22 @@ import CoreData
 import EssentialFeed
 import EssentialFeedAPI
 
+public extension HTTPClient {
+    typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
+
+    func getPublisher(from url: URL) -> Publisher {
+        var task: HTTPClientTask?
+
+        return Deferred {
+            Future { completion in
+                task = self.get(from: url, completion: completion)
+            }
+        }
+        .handleEvents(receiveCancel: { task?.cancel() })
+        .eraseToAnyPublisher()
+    }
+}
+
 public extension FeedImageDataLoader {
     typealias Publisher = AnyPublisher<Data, Error>
 
