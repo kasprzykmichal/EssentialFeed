62d36dcfbc9189a373d459441c3af06fc2935d85
Michał Kasprzyk <kasprzyk.michal@me.com>
2023-01-10 02:18:34 +0100

    Make RemoteLoader conform to FeedLoader in the Composition Root

--- EssentialFeed/EssentialFeedAPI/Shared API/RemoteLoader.swift
+++ EssentialFeed/EssentialFeedAPI/Shared API/RemoteLoader.swift
@@ -19,14 +19,14 @@ public final class RemoteLoader<Resource> {
     }
     
     public typealias Mapper = (Data, HTTPURLResponse) throws -> Resource
-    public typealias Result = Swift.Result<Resource, Error>
+    public typealias Result = Swift.Result<Resource, Swift.Error>
     
     public init(url: URL, client: HTTPClient, mapper: @escaping Mapper) {
         self.url = url
         self.client = client
         self.mapper = mapper
     }
-    
+
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard let self = self else { return }
