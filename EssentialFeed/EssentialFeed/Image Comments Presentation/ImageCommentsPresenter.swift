007d0e4dd15a11b5a0c366701daceb1b95c70043
Michał Kasprzyk <kasprzyk.michal@me.com>
2023-01-24 23:01:06 +0100

    Map ImageComments into ImageCommentsViewModels

--- EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -7,11 +7,39 @@
 
 import Foundation
 
+public struct ImageCommentsViewModel: Equatable {
+    public let comments: [ImageCommentViewModel]
+    
+    public init(comments: [ImageCommentViewModel]) {
+        self.comments = comments
+    }
+}
+
+public struct ImageCommentViewModel: Equatable {
+    public let message: String
+    public let date: String
+    public let username: String
+
+    public init(message: String, date: String, username: String) {
+        self.message = message
+        self.date = date
+        self.username = username
+    }
+}
+
 public class ImageCommentsPresenter {
     public static var title: String {
         return NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
             tableName: "ImageComments",
             bundle: Bundle(for: ImageCommentsPresenter.self),
             comment: "Title for the image comments view")
+    }
+
+    public static func map(_ comments: [ImageComment]) -> ImageCommentsViewModel {
+        let formatter = RelativeDateTimeFormatter()
+        
+        return ImageCommentsViewModel(comments: comments.map { comment in
+            return ImageCommentViewModel(message: comment.message, date: formatter.localizedString(for: comment.createdAt, relativeTo: .now), username: comment.username)
+        })
     }
 }
--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -26,14 +26,18 @@ extension Date {
     func minusFeedCacheMaxAge() -> Date {
         return adding(days: -feedCacheMaxAgeInDays)
     }
-    
-    private func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
 }
 
 extension Date {
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
+    }
+
+    func adding(minutes: Int) -> Date {
+        Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
+    }
+    
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
 }
