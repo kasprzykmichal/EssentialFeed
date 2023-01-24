4e4d702b67a7c68d78019e43e7da29fd6f404f38
Michał Kasprzyk <kasprzyk.michal@me.com>
2023-01-24 21:28:43 +0100

    Remove unused FeedPresenter logic

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -2,15 +2,15 @@
 //  FeedViewModel.swift
 //  EssentialFeed
 //
-//  Created by Michał Kasprzyk on 26/12/2022.
+//  Created by Michał Kasprzyk on 24/01/2023.
 //
 
 import Foundation
 
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public struct FeedViewModel {
     public let feed: [FeedImage]
+    
+    public init(feed: [FeedImage]) {
+        self.feed = feed
+    }
 }
