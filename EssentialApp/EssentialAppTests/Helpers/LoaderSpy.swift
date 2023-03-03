a8fd962079149cb21c4191f26ef9f89510fbd5e9
Michał Kasprzyk <kasprzyk.michal@me.com>
2023-03-03 14:20:29 +0100

    Make FeedImageDataLoader sync

--- EssentialApp/EssentialAppTests/Helpers/LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/LoaderSpy.swift
@@ -10,7 +10,7 @@ import EssentialFeed
 import EssentialFeedAPI
 import Combine
 
-class LoaderSpy: FeedImageDataLoader {
+class LoaderSpy {
     
     // MARK: - FeedLoader
     
@@ -62,33 +62,29 @@ class LoaderSpy: FeedImageDataLoader {
     }
     
     // MARK: - FeedImageDataLoader
-    
-    private struct TaskSpy: FeedImageDataLoaderTask {
-        let cancelCallback: () -> Void
-        
-        func cancel() {
-            cancelCallback()
-        }
-    }
-    
-    private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
-    private(set) var cancelledImageURLs = [URL]()
 
+    private var imageRequests = [(url: URL, publisher: PassthroughSubject<Data, Error>)]()
+            
     var loadedImageURLs: [URL] {
         return imageRequests.map { $0.url }
     }
-
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        imageRequests.append((url, completion))
-        return TaskSpy { [weak self] in self?.cancelledImageURLs.append(url) }
+            
+    private(set) var cancelledImageURLs = [URL]()
+    
+    func loadImageDataPublisher(from url: URL) -> AnyPublisher<Data, Error> {
+        let publisher = PassthroughSubject<Data, Error>()
+        imageRequests.append((url, publisher))
+        return publisher.handleEvents(receiveCancel: { [weak self] in
+            self?.cancelledImageURLs.append(url)
+        }).eraseToAnyPublisher()
     }
-
+    
     func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
-        imageRequests[index].completion(.success(imageData))
+        imageRequests[index].publisher.send(imageData)
+        imageRequests[index].publisher.send(completion: .finished)
     }
-
-    func completeImageLoadingWithError(at index: Int) {
-        let error = NSError(domain: "any error", code: 0)
-        imageRequests[index].completion(.failure(error))
+    
+    func completeImageLoadingWithError(at index: Int = 0) {
+        imageRequests[index].publisher.send(completion: .failure(anyNSError()))
     }
 }
--- EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataLoader.swift
@@ -8,7 +8,5 @@
 import Foundation
 
 public protocol FeedImageDataLoader {
-    typealias Result = Swift.Result<Data, Error>
-    
-    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+    func loadImageData(from url: URL) throws -> Data
 }
