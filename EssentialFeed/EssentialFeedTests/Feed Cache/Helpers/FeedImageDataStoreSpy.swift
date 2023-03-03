03a4575890a644255b9121270ba63f1f37d147b4
Michał Kasprzyk <kasprzyk.michal@me.com>
2023-03-03 13:44:53 +0100

    Load images synchronously

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -16,19 +16,19 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
     
     private(set) var receivedMessages = [Message]()
     
-    private var retrievalCompletions = [(RetrievalResult) -> Void]()
+    private var retrievalResult: Result<Data?, Error>?
     
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+    func retrieve(dataForURL url: URL) throws -> Data? {
         receivedMessages.append(.retrieve(dataFor: url))
-        retrievalCompletions.append(completion)
+        return try retrievalResult?.get()
     }
     
     func completeRetrieval(with error: Error, at index: Int = 0) {
-        retrievalCompletions[index](.failure(error))
+        retrievalResult = .failure(error)
     }
 
     func completeRetrieval(with data: Data?, at index: Int = 0) {
-        retrievalCompletions[index](.success(data))
+        retrievalResult = .success(data)
     }
 
     private var insertionResult: Result<Void, Error>?
